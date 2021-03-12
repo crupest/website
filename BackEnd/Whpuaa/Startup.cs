4fbd8022382a8d1bb460ff22f28940eccb92ddf1
crupest <crupest@outlook.com>
2021-03-12 14:43:42 +0800

    chore: Remove some unused things.

--- BackEnd/Whpuaa/Startup.cs
+++ BackEnd/Whpuaa/Startup.cs
@@ -30,7 +30,7 @@ namespace WhpuAA
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Whpuaa", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WhpuAA", Version = "v1" });
             });
         }
 
@@ -41,10 +41,8 @@ namespace WhpuAA
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Whpuaa v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WhpuAA v1"));
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
